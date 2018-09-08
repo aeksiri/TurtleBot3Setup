fca18019a8a4866d6566eee2e63b1f077467b143
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2018-09-08 16:43:01 +0700

    Add files via upload

---
+++ raspicam_autorace_pkgs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo
+echo
+echo "*******install raspicam packages***"
+echo
+echo
+
+
+cd ~/catkin_ws/src
+git clone https://github.com/UbiquityRobotics/raspicam_node.git
+sudo apt-get install ros-kinetic-compressed-image-transport ros-kinetic-camera-info-manager
+cd ~/catkin_ws && catkin_make
+
+echo
+echo "**** install autorace packages"
+echo
+
+cd ~/catkin_ws/src/
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_autorace.git
+cd ~/catkin_ws && catkin_make
+
+sudo apt-get install ros-kinetic-image-transport ros-kinetic-cv-bridge ros-kinetic-vision-opencv python-opencv libopencv-dev ros-kinetic-image-proc
+
+echo "******COMPLETE*****"
+
+
+
