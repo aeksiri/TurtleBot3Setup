f77f7e30bf84497c377e4ffec347619a28655d61
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2018-09-08 16:46:39 +0700

    Update raspicam_autorace_pkgs.sh

--- raspicam_autorace_pkgs.sh
+++ raspicam_autorace_pkgs.sh
@@ -9,7 +9,7 @@ echo
 
 cd ~/catkin_ws/src
 git clone https://github.com/UbiquityRobotics/raspicam_node.git
-sudo apt-get install ros-kinetic-compressed-image-transport ros-kinetic-camera-info-manager
+sudo apt-get install -y ros-kinetic-compressed-image-transport ros-kinetic-camera-info-manager
 cd ~/catkin_ws && catkin_make
 
 echo
@@ -20,7 +20,7 @@ cd ~/catkin_ws/src/
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_autorace.git
 cd ~/catkin_ws && catkin_make
 
-sudo apt-get install ros-kinetic-image-transport ros-kinetic-cv-bridge ros-kinetic-vision-opencv python-opencv libopencv-dev ros-kinetic-image-proc
+sudo apt-get install -y ros-kinetic-image-transport ros-kinetic-cv-bridge ros-kinetic-vision-opencv python-opencv libopencv-dev ros-kinetic-image-proc
 
 echo "******COMPLETE*****"
 
