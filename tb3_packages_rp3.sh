dd7eb2a6e370eeb00279d798e68ef928e73c5058
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2018-11-21 10:53:30 +0700

    Update tb3_packages_rp3.sh

--- tb3_packages_rp3.sh
+++ tb3_packages_rp3.sh
@@ -6,7 +6,7 @@ echo "******** install dependent packages *********"
 echo 
 echo
 
-sudo apt-get -y install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-interactive-markers ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation openssh-server terminator chromium-browser gedit
+sudo apt-get -y install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-interactive-markers ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation openssh-server terminator chromium-browser gedit ros-kinetic-mavros
 
 echo
 echo
