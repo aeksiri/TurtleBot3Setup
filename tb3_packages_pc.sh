13fabaa0bb650bf8957be0ce610974f3503a5d23
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2018-12-04 17:12:35 +0700

    Update tb3_packages_pc.sh

--- tb3_packages_pc.sh
+++ tb3_packages_pc.sh
@@ -6,7 +6,7 @@ echo "******** install dependent packages *********"
 echo 
 echo
 
-sudo apt-get -y install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-interactive-markers ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation openssh-server terminator chromium-browser gedit ros-kinetic-mavros apache2
+sudo apt-get -y install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-interactive-markers ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation openssh-server terminator chromium-browser gedit ros-kinetic-mavros apache2 python-pip
 
 echo
 echo
@@ -64,6 +64,7 @@ git clone https://github.com/aeksiri/download_google_drive.git
 cd download_google_drive
 chmod +x download_gdrive.py
 pip install tqdm
+pip install requests
 python download_gdrive.py 1ib5wb3TMpGUChPnR_sKnZO0-8oH-qhk4 ~/catkin_ws/src/turtlebot3_blockly.tar.gz
 cd ~/catkin_ws/src
 tar --extract -f turtlebot3_blockly.tar.gz
